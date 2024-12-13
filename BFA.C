c0a1870d8e610042cca8d0f559f738b8095f7f4b
nihajangili <jangiliniharika6@gmail.com>
2024-12-13 14:49:35 +0530

    Add files via upload

---
+++ BFA.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+int l;
+char str[20];
+clrscr();
+printf("\n enter the string:");
+scanf("%s",str);
+l=strlen(str);
+printf("\n the length of the string is l=%d",l);
+printf("\n the given string acceptable");
+getch();
+}
+
